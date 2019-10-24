3db6e975b01b166a7f208f93a08446d9ee268163
sujo028 <48432120+sujo028@users.noreply.github.com>
2019-10-24 21:09:26 +0530

    Update count .c
    Count number of characters in a String .

--- count.py
+++ count.py
@@ -1,3 +1,9 @@
-l=list(input().split())
-count=len(l)
-print count
+#include<stdio.h>
+void main(){
+  int count;
+  char l[100];
+  printf("Enter the string:");
+  gets(l);
+  count=strlen(l)
+  printf("length of string=%d",count);
+}
